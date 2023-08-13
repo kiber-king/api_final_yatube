fb34a097ce5b814dcde8fc9cabb36cbb770dfbe6
Gleb <gleb.kvasov26@mail.ru>
2023-08-13 23:34:22 +0300

    new changes

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,11 +5,9 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (
                 request.method in SAFE_METHODS
-                or request.user.is_authenticated
-        )
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (
                 request.method in SAFE_METHODS
-                or obj.author == request.user
-        )
+                or obj.author == request.user)
